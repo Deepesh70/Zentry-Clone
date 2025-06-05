666ee85cdc2e13b7362a2e183f4c4c0949f659cd
Deepesh <deepeshdangi700@gmail.com>
2025-06-05 22:41:29 +0530

    GamingWeb

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className= "text-5xl  text-violet-500 text-orange-500 font-bold text-blue-50">Hello World H</h1>
+    </main>
+  )
+} 
+
+export default App
