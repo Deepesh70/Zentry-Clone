ab05a39f1ec259915b3264015a7cc5629bf069b9
Deepesh <deepeshdangi700@gmail.com>
2025-06-22 15:01:11 +0530

    Feature Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Feature from './components/Feature'
 const App = () => {
   return (
       <main className="min-h-screen overflow-x-hidden">
         <Navbar />
         <Hero />
         <About />
-       
+        <Feature />
       </main>
   )
 }
